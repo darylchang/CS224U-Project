be6d537d6f16369f66c6c83fcc4e2c4c039b0d46
akma327 <akma327@stanford.edu>
2015-05-22 18:50:26 -0700

    Wrote the RakeMatrix coocurrence dictionary generator that is based on splitting the text upon specified delimeters.

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 import networkx
 import numpy as np 
 from collections import defaultdict
+from parse import parse 
 
 ####### Create the adjacency matrix first and then a graph based on it #######
 #cooccurence by N sized sliding window
@@ -8,15 +9,6 @@ from collections import defaultdict
 #RAKE, use delimitors to split the text upon stop word set. Coccurence 
 #between word (A,B) is if they occur in the chunks together. 
 
-
-def textToWordArray(fileName):
-	wordArray = []
-	with open(fileName) as f:
-		for line in f:
-			lineparse = line.lower().split()
-			for word in lineparse:
-				wordArray.append(word.strip('.').strip(','))
-	return wordArray
 
 def slidingWindowMatrix(wordList, N):
 	numWords = len(wordList)
@@ -35,9 +27,28 @@ def slidingWindowMatrix(wordList, N):
 				cooccurenceDict[(window[i1], window[i2])] += 1
 	return cooccurenceDict
 
+def RakeMatrix(wordList, delimiters):
+	fragments = []
+	beg = 0
+	for idx, val in enumerate(wordList):
+		if wordList[idx] in delimiters:
+			frag = wordList[beg:idx]
+			fragments.append(frag)
+			beg = idx+1
+	cooccurenceDict = defaultdict(lambda:0)
+	for frag in fragments:
+		window = list(set(frag))
+		window_len = len(window)
+		for i1 in range(0, window_len):
+			for i2 in range(i1, window_len):
+				cooccurenceDict[window[i1], window[i2]] += 1
+	return cooccurenceDict, len(cooccurenceDict)
+
+fileName = 'data/yeastsamp.txt'
+# windowWordArray = parse(fileName)
+# print slidingWindowMatrix(windowWordArray, 5)
+rakeWordArray = parse(fileName, stripPunct=False)
+delimiters = set(['.', ',', 'the'])
+print RakeMatrix(rakeWordArray, delimiters)
 
 
-fileName = 'yeastsamp.txt'
-wordArray = textToWordArray(fileName)
-#print(wordArray)
-print slidingWindowMatrix(wordArray, 5)
