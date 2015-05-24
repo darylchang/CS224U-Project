e6f05526811cabc216badee2394a681f25b7f464
Keith Wyngarden <kwyngard@stanford.edu>
2015-05-24 11:05:41 -0700

    Add data and basic evaluation framework

--- parse.py
+++ parse.py
@@ -2,36 +2,9 @@ import nltk
 from nltk.corpus import wordnet
 from nltk.tokenize import RegexpTokenizer
 
-############ Parse.py ####################
-# Parses text documents into a list of tokens.
-# Handles multiple file formats.
-
-def parse(filename, stripPunct=True, stemRule=None, lemmatize=True):
-	with open (filename, 'r') as f:
-		text = f.read()
-
-		# Strip punctuation if unneeded for co-occurrence counts
-		if stripPunct:
-			tokenizer = RegexpTokenizer(r'\w+')
-			tokens = tokenizer.tokenize(text)
-		else:
-			tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-
-		# Normalize words
-		words = [t.lower() for t in tokens]
-		
-		# Stemming. Faster than lemmatization but imprecise
-		if stemRule:
-			stemmer = nltk.PorterStemmer() if stemRule=='Porter' else nltk.LancasterStemmer()
-			words = [stemmer.stem(w) for w in words]
-
-		# Lemmatization.
-		if lemmatize:
-			lemmatizer = nltk.WordNetLemmatizer()
-			taggedWords = nltk.pos_tag(words)
-			words = [lemmatizer.lemmatize(word, wordnetPosCode(tag)) for word, tag in taggedWords]
-
-		return words
+#################### Parse.py ####################
+# Contains readers that convert the various data sets into lists of
+# (tokens, keywords) pairs.
 
 # Maps from NLTK POS tags to WordNet POS tagsC
 def wordnetPosCode(tag):
@@ -46,8 +19,45 @@ def wordnetPosCode(tag):
     else:
         return wordnet.NOUN
 
-def main():
-	parse('data/litReview.txt', lemmatize=True)
+def tokenize(text, stripPunct=True, stemRule=None, lemmatize=True):
+    # Strip punctuation if unneeded for co-occurrence counts
+    if stripPunct:
+        tokenizer = RegexpTokenizer(r'\w+')
+        tokens = tokenizer.tokenize(text)
+    else:
+        tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+
+    # Normalize words
+    words = [t.lower() for t in tokens]
+    
+    # Stemming. Faster than lemmatization but imprecise
+    if stemRule:
+        stemmer = nltk.PorterStemmer() if stemRule=='Porter' else nltk.LancasterStemmer()
+        words = [stemmer.stem(w) for w in words]
+
+    # Lemmatization.
+    if lemmatize:
+        lemmatizer = nltk.WordNetLemmatizer()
+        taggedWords = nltk.pos_tag(words)
+        words = [lemmatizer.lemmatize(word, wordnetPosCode(tag)) for word, tag in taggedWords]
+
+    return words
+
+def handwritten_data_reader(**kwargs):
+    # Simple data format for handwritten document/keyword examples. The first
+    # line of each file listed in handwritten_docs.txt is taken to be 
+    data_dir = 'data/Handwritten/'
+    with open(data_dir + 'handwritten_docs.txt', 'r') as docs_file:
+        filenames = [filename.strip() for filename in docs_file.readlines()]
+
+    examples = []
+    for filename in filenames:
+        with open(data_dir + filename, 'r') as f:
+            label_line = f.readline().decode('utf-8')
+            text = f.read().decode('utf-8')
+        labels = set([label.strip().lower() for label in label_line.split(';')])
+        examples.append((tokenize(text, **kwargs), labels))
+    return examples
 
 if __name__=='__main__':
-	main()
+    print handwritten_data_reader(lemmatize=False)
