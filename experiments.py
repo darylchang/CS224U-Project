95d47314d96b8cf40a29fcec96f0d7a96f658150
Keith Wyngarden <kwyngard@stanford.edu>
2015-05-29 20:18:05 -0700

    Add experiments

---
+++ experiments.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
+
+from parse import *
+
+
+def count_keyphrase_lengths():
+    examples = inspec_data_reader() + duc_data_reader()
+    counter = Counter()
+    ngram_in_document = Counter()
+    words_in_document = Counter()
+    not_in_document = Counter()
+    labels_per_document = Counter()
+    lengths = []
+    doc_lengths = []
+    label_counts = []
+
+    for filename, text, labels in examples:
+        for label in labels:
+            words = label.split()
+            num_tokens = len(words)
+            lengths.append(num_tokens)
+            counter[num_tokens] += 1
+
+            if label in text:
+                ngram_in_document[num_tokens] += 1
+            elif all([word in text for word in words]):
+                words_in_document[num_tokens] += 1
+            else:
+                not_in_document[num_tokens] += 1
+
+        label_counts.append(len(labels))
+        labels_per_document[len(labels)] += 1
+        doc_lengths.append(len(text.split()))
+    
+    for token_length in sorted(counter):
+        print '%s tokens: %s keyphrases' % (token_length, counter[token_length])
+        print '\tNgram occurred in document: %4s (%.2f%%)' % (ngram_in_document[token_length], 100.0 * ngram_in_document[token_length] / counter[token_length])
+        print '\tWords occurred in document: %4s (%.2f%%)' % (words_in_document[token_length], 100.0 * words_in_document[token_length] / counter[token_length])
+        print '\tDid not occur in document:  %4s (%.2f%%)' % (not_in_document[token_length], 100.0 * not_in_document[token_length] / counter[token_length])
+
+    print '\n'
+
+    for num_labels in sorted(labels_per_document):
+        print '%2s keyphrases: %4s documents' % (num_labels, labels_per_document[num_labels])
+
+    plt.figure(0)
+    plt.hist(lengths, bins=9)
+    plt.xlabel("Words in keyphrase")
+    plt.ylabel("Keyphrase count")
+
+    plt.figure(1)
+    plt.hist(label_counts, bins=31)
+    plt.xlabel("Number of keyphrases")
+    plt.ylabel("Documents")
+
+    plt.figure(2)
+    plt.scatter(doc_lengths, label_counts)
+    plt.axis([0, 1800, 0, 25])
+    plt.xlabel("Document length")
+    plt.ylabel("Number of keyphrases")
+
+    plt.show()
+
+if __name__=='__main__':
+    count_keyphrase_lengths()
