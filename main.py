a8fa3af9d839de6216825bf7c5b6ad4bb16322f5
Daryl Chang <darylc123@gmail.com>
2015-05-30 20:26:31 -0700

    Added gridsearch

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from degreeCentralityModel import DegreeCentralityModel
 from pageRankModel import PageRankModel
 from nltk.corpus import wordnet
 import nltk
+from gridSearch import gridSearch
 
-model = DegreeCentralityModel(windowSize=10, synFilter=[wordnet.NOUN, wordnet.ADJ])
-model.evaluate(numExamples=100, verbose=False)
+# model = DegreeCentralityModel(windowSize=10, synFilter=[wordnet.NOUN, wordnet.ADJ], 
+# 							  keywordThreshold=5)
+# model.evaluate(numExamples=10, verbose=False)
+
+options = dict()
+options['windowSize']=[5,10,15]
+options['keywordThreshold']=[4,5,6]
+gridSearch(DegreeCentralityModel, 3, options)
