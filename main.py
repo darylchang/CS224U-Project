ff51696ce1b5bc4ab4ce8cc6decb4364fdcfd599
Daryl Chang <darylc123@gmail.com>
2015-06-02 15:55:20 -0700

    Modified params for gridSearch

--- main.py
+++ main.py
@@ -19,29 +19,30 @@ trigrams = ngrams.read_trigrams()
 # results = model.evaluate(numExamples=5, compute_mistakes=True, verbose=False)
 
 
-lengthPenaltyFn = lambda x: x**3/60. if x<4 else x/3.
-ngramPenaltyFn = lambda length, count: 0.3 * float(length) / np.sqrt(count)
-ngramAdjacentBoostFn = lambda length, count: np.sqrt(length * count) / 50.
+# lengthPenaltyFn = lambda x: x**3/60. if x<4 else x/3.
+# ngramPenaltyFn = lambda length, count: 0.3 * float(length) / np.sqrt(count)
+# ngramAdjacentBoostFn = lambda length, count: np.sqrt(length * count) / 50.
 
-model = DegreeCentralityModel(lengthPenaltyFn=lengthPenaltyFn, useNgrams=[bigrams, trigrams], ngramPenaltyFn=ngramPenaltyFn, ngramAdjacentBoostFn=ngramAdjacentBoostFn, keywordThreshold=10)
-model.evaluate(numExamples=50, compute_mistakes=True, verbose=False)
+# model = DegreeCentralityModel(lengthPenaltyFn=lengthPenaltyFn, useNgrams=[bigrams, trigrams], ngramPenaltyFn=ngramPenaltyFn, ngramAdjacentBoostFn=ngramAdjacentBoostFn, keywordThreshold=10)
+# model.evaluate(numExamples=50, compute_mistakes=True, verbose=False)
+
+options = dict()
+options['model'] = [DegreeCentralityModel]
+options['useNgrams'] = [[bigrams, trigrams]]
 
 
-# options = dict()
-# options['model'] = [DegreeCentralityModel]
-# # options['windowSize'] = [22, 24, 26, 28]
+options['windowSize'] = [10, 15, 25]
+options['keywordThreshold'] = [3, 4, 5]
 
-# powers = [3]
-# firstDenoms = [40.]
-# secondDenoms = [2.]
+options['ngramPenaltyParams'] = [0.05, 0.3, 0.6,]
+options['ngramAdjacentBoostParams'] = [1/75., 1/50., 1/25.,]
 
-# combos = itertools.product(powers,firstDenoms,secondDenoms)
-# options['lengthPenaltyParams'] = combos
-# # options['keywordThreshold'] = [2, 3, 4, 5]
+powers = [3]
+firstDenoms = [60.]
+secondDenoms = [3.]
 
-# options['useNgrams'] = [[bigrams, trigrams]]
-# options['ngramPenaltyParams'] = [0.25,0.5]
-# options['ngramAdjacentBoostParams'] = [1/50.]
 
-# use_datasets = ['Inspec', 'DUC-2001']
-# gridSearch(options, use_datasets=use_datasets, numExamples=5, verbose=True)
+combos = itertools.product(powers,firstDenoms,secondDenoms)
+options['lengthPenaltyParams'] = combos
+use_datasets = ['DUC-2001']
+gridSearch(options, use_datasets=use_datasets, numExamples=None, verbose=True)
